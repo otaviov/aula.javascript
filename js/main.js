dddfe19561920e328e33e61378362ad769dde73f
otaviov <amandavv32@gmail.com>
2021-02-23 15:41:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,19 @@ function soma  (n1, n2){
     return n1+ n2;
 }
 
-function setReplace(frase, nome){
-    frase.replace(nome, novo_nome);
+function validaIdade(idade){
+    var validar;
+    if(idade>= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
 }
- alert (soma(5, 10));
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+
 
 
 
